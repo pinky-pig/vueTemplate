02b4e522777e60e70f30a0b3683f301fa97a40f3
pinky-pig <2545589993@qq.com>
2021-07-02 17:55:24 +0800

    全景修改

--- src/views/pano/SceneList.vue
+++ src/views/pano/SceneList.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2021-07-02 15:50:30
  * @LastEditors: wangwenbo
- * @LastEditTime: 2021-07-02 17:00:04
+ * @LastEditTime: 2021-07-02 17:55:03
  * @FilePath: \vuetemplate\src\views\pano\SceneList.vue
 -->
 <template>
   <div :class="$style.sceneList"
     class=" w-full h-32 bg-black bg-opacity-50 py-2 space-x-4 flex flex-row justify-center">
     <div class=" bg-white bg-opacity-25 w-32 h-full " v-for="(scene,index) in sceneList" :key="index">
-      <img :src="scene.thumburl" alt="" class="object-cover w-full h-full">
+      <img :src="scene.thumburl" alt="" class="object-cover w-full h-full cursor-pointer" @click="changeScene(scene)">
     </div>
   </div>
 </template>
@@ -23,10 +23,19 @@ export default {
       }]
     }
   },
+  data() {
+    return {
+      krpano: document.querySelector('#krpanoSWFObject'),
+    }
+  },
   mounted() {
     console.log(this.sceneList);
   },
   methods: {
+    changeScene(scene){
+      var rand = Math.floor(Math.random() * 10 + 1);
+      this.krpano.call('loadscene(' + scene.name + `,null,MERGE,get(blendmodes[transition${rand}].blend))`);
+    }
 
   },
 }
